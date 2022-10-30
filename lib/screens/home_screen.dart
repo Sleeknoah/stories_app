b3c0ec3407c794d1350fa7401befdbeeeaaa979a
Sleeknoah <yungdyke@gmail.com>
2022-10-31 00:07:41 +0100

    Status Page/ Implement state mangement

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:stories_app/model/repository/status_repository.dart';
+import 'package:stories_app/model/service/status_service.dart';
+import 'package:stories_app/viewmodel/providers/providers.dart';
+
+import '../model/response/data.dart';
+import '../utils/friends_status_container.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({
@@ -16,10 +23,23 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   void initState() {
     super.initState();
     // "ref" can be used in all life-cycles of a StatefulWidget.
+    ///Retrieve data on start up
+    ref
+        .read(statusProvider.notifier)
+        .retrieveStatus(StatusRepository(statusService: StatusService()));
   }
 
   @override
   Widget build(BuildContext context) {
+    final loadingState = ref.watch(loadingProvider);
+    final statusState = ref.watch(statusProvider);
+    ref.listen<Data?>(statusProvider, (previous, next) {
+      ///if there is data set loading to false state
+      if(next != null){
+        ref.read() = false;
+      }
+    });
+
     return Scaffold(
       body: SafeArea(
         child: ListView(
@@ -67,6 +87,46 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
             ///Sized Box
             const SizedBox(
               height: 30,
+            ),
+
+            ///Staggered GridView done here
+            Column(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Visibility(
+                      visible: loadingState,
+                      child: const CircularProgressIndicator(),
+                    ),
+                  ),
+                ),
+                Visibility(
+                  visible: !loadingState,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Expanded(
+                      child: MasonryGridView.builder(
+                        gridDelegate:
+                            const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2),
+                        shrinkWrap: true,
+                        primary: false,
+                        crossAxisSpacing: 16,
+                        mainAxisSpacing: 16,
+                        itemCount: 10,
+                        itemBuilder: (BuildContext context, int index) {
+                          return const FriendStatus(
+                            imgUrl:
+                                "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
+                            avatarUrl: "avatarUrl",
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
